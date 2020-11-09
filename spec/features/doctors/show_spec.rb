3628b2de7fb1c212025b551957ee0f91ff4d3277
Eugene Theriault <hybridbassist@gmail.com>
2020-11-09 08:38:36 -0800

    Create test for hospital and patients names on doctor show page

--- spec/features/doctors/show_spec.rb
+++ spec/features/doctors/show_spec.rb
@@ -19,4 +19,18 @@ describe 'As a visitor when I visit a doctors show page' do
     expect(page).to have_content(@doctor.specialty)
     expect(page).to have_content(@doctor.university)
   end
+
+  it "I see that doctors hospital and all their patients" do
+    visit "/doctors/#{@doctor.id}"
+
+    expect(page).to have_content(@hospital.name)
+
+    within("#patient-#{@patient1.id}") do
+      expect(page).to have_content(@patient1.name)
+    end
+
+    within("#patient-#{@patient2.id}") do
+      expect(page).to have_content(@patient2.name)
+    end
+  end
 end
