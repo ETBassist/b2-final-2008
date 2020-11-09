1dd0dcaadd0a674129ea001886557cc4f8a7d944
Eugene Theriault <hybridbassist@gmail.com>
2020-11-09 11:01:02 -0800

    Update with additional param for finding doctor_patient

--- app/controllers/doctor_patients_controller.rb
+++ app/controllers/doctor_patients_controller.rb
@@ -1,6 +1,6 @@
 class DoctorPatientsController < ApplicationController
   def destroy
-    doctor_patient = DoctorPatient.find_by(patient_id: params[:patient_id])
+    doctor_patient = DoctorPatient.find_by(patient_id: params[:patient_id], doctor_id: params[:doctor])
     doctor_patient.destroy
     redirect_to "/doctors/#{doctor_patient.doctor_id}"
   end
